db3e1f0769631577001b29d998a27bacbab9f7b9
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-24 16:21:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,5 +13,6 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index {|name, index| return "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+  new_arr = []
+  attendees.each_with_index {|name, index|  new_arr << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
 end
